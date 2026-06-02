3b4331ea24174bd98ec86a9e2628acdcedcbc8eb
ttklinh1 <49190900+ttklinh1@users.noreply.github.com>
2026-06-02 20:37:21 +0700

    report integration

--- my-app/src/main/java/base/BaseTest.java
+++ my-app/src/main/java/base/BaseTest.java
@@ -1,6 +1,13 @@
 package base;
 
 import driver.DriverManager;
+import io.qameta.allure.Allure;
+
+import java.io.ByteArrayInputStream;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -12,7 +19,17 @@ public class BaseTest {
     }
 
     @AfterMethod
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+        byte[] screenshot =
+            ((TakesScreenshot) DriverManager.getDriver())
+                .getScreenshotAs(OutputType.BYTES);
+
+        Allure.addAttachment(
+            "Failure Screenshot",
+            new ByteArrayInputStream(screenshot));
+    }
+
         DriverManager.quit();
     }
 }
